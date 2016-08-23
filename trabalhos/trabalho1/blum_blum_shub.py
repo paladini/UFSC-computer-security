fe695da5ea4e31562b0c836c5405131157bbc947
Fernando Paladini <fnpaladini@gmail.com>
2016-08-23 06:47:23 -0300

    Documenting everything.

--- trabalhos/trabalho1/blum_blum_shub.py
+++ trabalhos/trabalho1/blum_blum_shub.py
@@ -6,9 +6,45 @@ import time
 from lcg import lcg
 from primality import MillerRabin 
 
+"""
+
+Esta classe gera números pseudo-aleatórios utilizando o algoritmo BBS (Blum Blum Shub).
+Para chamar esta classe a partir da linha de comando basta digitar:
+    
+    $ python blum_blum_shub.py <qtd_de_bits>
+
+Onde "<qtd_de_bits>" é a quantidade de bits que o número gerado deve possuir. Exemplo:
+
+    $ python blum_blum_shub.py 32
+    [Blum Blum Shub] Gerando número de 32 bits...
+    [Blum Blum Shub] Número: 3234751506
+
+Referências:
+    https://en.wikipedia.org/wiki/Blum_Blum_Shub
+    https://pt.wikipedia.org/wiki/Blum_Blum_Shub
+    https://crypto.stackexchange.com/questions/3454/blum-blum-shub-vs-aes-ctr-or-other-csprngs
+    https://jeremykun.com/2016/07/11/the-blum-blum-shub-pseudorandom-generator/
+    http://cs.ucsb.edu/~koc/cren/project/pp/gawande-mundle.pdf
+
+"""
 class BlumBlumShub(object):
 
     def __init__(self, seed = None, size = None):
+        """
+        Construtor da classe do gerador de números pseudo-aleatórios.
+
+        Por padrão vai gerar um número pseudo-aleatório de até 32 bits, mas caso um "size" seja fornecido, 
+        vai gerar somente números com "size" bits.
+
+        Args:
+            seed: valor de semente para iniciar o gerador de números pseudo-aleatórios. Se não 
+                  informado, utiliza um valor numérico pseudo-aleatório entre 2 e (m - 1).
+            size: o tamanho em bits do número a ser gerado, cujo valor padrão é None (na prática é 32).
+
+        Returns:
+            Esse método não retorna nada.
+                
+        """
         self.size = size
         self.m = MillerRabin.encontrar_primo(self.size) * MillerRabin.encontrar_primo(self.size)
         if (seed):
@@ -17,6 +53,33 @@ class BlumBlumShub(object):
             self.state = lcg(size=self.size).randint(2, self.m - 1) % self.m
     
     def rand(self):
+        """
+        Gera um número aleatório utilizando a fórmula para calcular a LCG, que é descrita pela relação
+        de recorrência expressa a seguir:
+
+            Xn+1 = (a * Xn + c) mod m
+
+        Onde: 
+            m: o módulo (0 < m)
+            a: o multiplicador (0 < a < m)
+            c: o incremento (0 <= c < m)
+            X: sequência de valores pseudo-aleatórios.
+            X0: o "seed" ou valor de começo.
+            Xn+1: o próximo número a ser gerado.
+
+        Se uma instância dessa classe possuir o atributo "size" definido no momento da construção do objeto
+        ou definido posteriormente em uma chamada ao método seed(self, new_seed), então o número gerado de 
+        forma pseudo-aleatória possuirá "size" bits (ficará dentro de um loop enquanto não atingir essa 
+        quantidade de bits estipulada). Caso a instância não possua o atributo "size" definido, então o
+        número pseudo-aleatório gerado possuirá até 32 bits.
+
+        Args:
+            Este método não recebe nenhum argumento.
+
+        Returns:
+            Um valor numérico pseudo-aleatório de "size" bits.
+
+        """
         output_bits = ''
         for bit in self.bitstream():
             output_bits += str(bit)
@@ -26,7 +89,32 @@ class BlumBlumShub(object):
         return int(output_bits, 2)
 
     def seed(self, new_seed):
+        """
+        Método para mudar o valor do seed para algum valor desejado.
+
+        Args:
+            new_seed: um valor numérico para indicar o novo valor de seed.
+
+        Returns:
+            Esse método não retorna nada.
+
+        """
         self.state = new_seed
+
+    def size(self, new_size):
+        """
+        Método para mudar a quantidade de bits que o número gerado terá.
+
+        Args:
+            new_size: um valor numérico para indicar a nova quantidade de bits do número gerado.
+
+        Returns:
+            Esse método não retorna nada.
+
+        """
+        if self.size != new_size:
+            self.size = new_size
+            self.m = 2**new_size
     
     def bitstream(self):
         while (True):
--- trabalhos/trabalho1/primality.py
+++ trabalhos/trabalho1/primality.py
@@ -5,10 +5,56 @@ import sys
 import time
 from lcg import lcg
 
+"""
+
+As classes deste arquivo verificam se os números fornecidos são primos e geram números 
+primos de tamanho variável de bits de forma pseudo-aleatória.
+
+Para executar esse arquivo a partir da linha de comando basta digitar:
+    
+    $ python primality.py <qtd_de_bits>
+
+Onde "<qtd_de_bits>" é a quantidade de bits que o número gerado deve possuir. Exemplo:
+
+    $ python primality.py 128
+    [MillerRabin] Procurando primo...
+    [MillerRabin] 304159568226184448912103696911866306307 é primo!
+    [Fermat] Procurando primo...
+    [Fermat] 301970148924118150955226359108149582211 é primo!
+
+Referências:
+    https://en.wikipedia.org/wiki/Fermat%27s_little_theorem
+    https://en.wikipedia.org/wiki/Pseudoprime
+    https://pt.wikipedia.org/wiki/Teste_de_primalidade_de_Miller-Rabin
+    https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+    https://pt.wikipedia.org/wiki/Teste_de_primalidade_de_Fermat
+    https://jeremykun.com/2013/06/16/miller-rabin-primality-test/
+    http://mathworld.wolfram.com/Rabin-MillerStrongPseudoprimeTest.html
+    https://www.youtube.com/watch?v=qfgYfyyBRcY
+
+"""
 class MillerRabin(object):
 
     @staticmethod
     def verificar_testemunha(possivel_testemunha, p, exp, resto):
+        """
+        Verifica se a possível testemunha de que um número não é primo é
+        realmente testemunha. Se for, significa que o número que está 
+        sendo testado tem uma testemunha da sua não primalidade, de forma
+        que a hipótese de que o número testado é primo pode ser descartada.
+
+        Args:
+            possivel_testemunha: a possível testemunha de que "p" não é primo.
+                                 Será testemunha caso a^d ≢ 1 (mod n) e 
+                                 a^((2^r)d) ≢ -1 (mod n) para todo 0 <= r <= s - 1.
+            p: o número para o qual a primalidade está sendo testada.
+            exp, resto: números inteiros, onde 'resto' é um número ímpar.
+
+        Returns:
+            True se a possível testemunha for uma testemunha de que "p" não é primo.
+            False se a possível testemunha não for uma testemunha de verdade.
+
+        """
         possivel_testemunha = pow(possivel_testemunha, resto, p)
         if ((possivel_testemunha == 1) or (possivel_testemunha == p - 1)):
             return False
@@ -22,6 +68,25 @@ class MillerRabin(object):
     
     @staticmethod
     def verificar_primalidade(p, certeza=100):
+        """
+        O teste de Miller-Rabin é um importantíssimo teste probabilístico da primitividade de um número dado.
+        Se um número passa nesse teste significa que ele tem uma probabilidade >= 75% de ser um número primo,
+        mas até este número ser provado como sendo um número primo ele é considerado apenas um "pseudoprimo".
+
+        Ao aplicar o mesmo teste várias vezes, a margem de erro pode ser diminuída aleatoriamente, de forma
+        que a margem de erro final seja consideravelmente baixa. Ele é baseado no "Pequeno Teorema de Fermat",
+        que consiste do "Teste de primalidade de Fermat".
+
+        Args:
+            p: o número a ser testado.
+            certeza: o grau de "certeza" de que este número seja de fato um número primo. Valor padrão é 100, o
+                     que significa que o teste será aplicado 100 vezes.
+
+        Returns:
+            True se o número é um (pseudo-)primo.
+            False se o número não é primo.
+
+        """
         if (p == 2 or p == 3): 
             return True
         elif (p < 2): 
@@ -42,6 +107,16 @@ class MillerRabin(object):
 
     @staticmethod
     def encontrar_primo(bits=None):
+        """
+        Encontra um número primo que possui 'bits' bits utilizando uma busca com números gerados de forma pseudo-aleatória. 
+
+        Args:
+            bits: quantidade de bits que o número primo deve possuir. Se nenhum valor for informado, será usado 32 bits.
+        
+        Returns:
+            Um valor númerico com 'bits' bits e que é primo.
+
+        """
         bits = bits or 32
         random = lcg(size=bits)
 
@@ -54,6 +129,24 @@ class FermatPrimality(object):
 
     @staticmethod
     def verificar_primalidade(p):
+        """
+        Verifica se o número dado 'p' é um número primo através do método de Fermat, também conhecido 
+        como "Teste de Primalidade de Fermat". Este é um dos métodos mais simples para verificar se um 
+        número é primo ou não (e provavelmente um dos mais elegantes também). Neste método a composição
+        do número dado é verificada e os números que falham no teste não são primos.
+
+        Esta implementação não leva em consideração os números de Carmichael, que são infinitos e
+        passam pelo teste, mas não são primos. Portanto, a partir deste teste podemos obter apenas
+        números que são considerados "pseudoprimos".
+
+        Args:
+            p: o número que será testado a primalidade.
+
+        Returns:
+            True significa que o número é pseudoprimo, ou seja, provavelmente é primo (existem falso-positivos).
+            False significa que o número é composto, ou seja, não é primo.
+
+        """
         if (p == 2):
             return True
         if (not p & 1):
@@ -66,6 +159,16 @@ class FermatPrimality(object):
 
     @staticmethod
     def encontrar_primo(bits=None):
+        """
+        Encontra um número primo que possui 'bits' bits utilizando uma busca com números gerados de forma pseudo-aleatória. 
+
+        Args:
+            bits: quantidade de bits que o número primo deve possuir. Se nenhum valor for informado, será usado 32 bits.
+        
+        Returns:
+            Um valor númerico com 'bits' bits e que é primo.
+
+        """
         bits = bits or 32
         random = lcg(size=bits)
 
@@ -80,7 +183,7 @@ if (__name__ == "__main__"):
 
     print("[MillerRabin] Procurando primo...")
     print("[MillerRabin] {} é primo!".format(MillerRabin.encontrar_primo(bits)))
-    
+
     time.sleep(2)
     
     print("[Fermat] Procurando primo...")
